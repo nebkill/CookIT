10e202a2896a4adc22e46361edda1c2820dd29d0
Mike de Haas <I414486@fhict.nl>
2018-12-05 15:01:40 +0100

    User klasse constructor, getters en setters gemaakt

--- CookIT/User.cs
+++ CookIT/User.cs
@@ -8,5 +8,15 @@ namespace CookIT
 {
     class User
     {
+        private string naam { get; set; }
+        private string achternaam { get; set; }
+        private string email { get; set; }
+
+        public User(string naam, string achternaam, string email)
+        {
+            this.naam = naam;
+            this.achternaam = achternaam;
+            this.email = email;
+        }
     }
 }
