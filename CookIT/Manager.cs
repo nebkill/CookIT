7775e5f546b133f8d6e72f229b91ec8cf08c3141
MichaJDevLaptop <vyxisprime@gmail.com>
2018-12-05 15:38:47 +0100

    Connect.Cs insertRecept Method frame Handler added insertRecept voor communicatie met de database Recept variables set to public; able to get/set.

--- CookIT/Manager.cs
+++ CookIT/Manager.cs
@@ -8,5 +8,14 @@ namespace CookIT
 {
     class Manager
     {
+        Connect con = new Connect();
+        /**
+         * Gebruik deze functie om met de database to communiceren om 
+         * nieuwe recepten aan de recepten table toe te voegen.
+         **/
+        public void insertRecept(Recept recept)
+        {
+            con.insertRecept(recept);
+        }
     }
 }
