6a0b414a588ef43cb738528d4e6c11bc5910f8aa
MichaJDevLaptop <vyxisprime@gmail.com>
2018-12-05 15:30:53 +0100

    Added Connection methods to Connect.cs They only need to be filled.

--- CookIT/Connect.cs
+++ CookIT/Connect.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,48 @@ namespace CookIT
 {
     class Connect
     {
+        string server, database, uid, password;
+        MySqlConnection connection;
+        public Connect()
+        {
+            server = "";
+            database = "";
+            uid = "";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+
+            }
+            return false;
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return false;
+            }
+        }
     }
 }
