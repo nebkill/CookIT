2b077142cc2174d33e3c0719ee61852a550c6b35
Mike de Haas <I414486@fhict.nl>
2018-12-05 13:49:58 +0100

    Added all User conrols

---
+++ CookIT/Module_ReceptAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookIT
+{
+    public partial class Module_ReceptAdd : UserControl
+    {
+        public Module_ReceptAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
