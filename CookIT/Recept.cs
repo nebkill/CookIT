f94ef8a95b3ec1ce98bb1e3023646d2be3bcc4f5
Mike de Haas <I414486@fhict.nl>
2018-12-05 14:56:23 +0100

    Recept klasse constructor en getters en setters gemaakt. User klasse toegevoegd

--- CookIT/Recept.cs
+++ CookIT/Recept.cs
@@ -8,5 +8,30 @@ namespace CookIT
 {
     class Recept
     {
+
+        private string naam { get; set; }
+        private string desc { get; set; }
+        private string auteur { get; set; }
+        private string video { get; set; }
+        private string rating { get; set; }
+        private string dieet { get; set; }
+        private string[] benodigdheden { get; set; }
+        private string image { get; set; }
+        private string[] ingredienten { get; set; }
+        List<string> stappen { get; set; }
+
+        public Recept(string naam, string desc, string auteur, string video, string rating, string dieet, string[] benodigdheden, string image, string[] ingredienten, List<string> stappen)
+        {
+            this.naam = naam;
+            this.desc = desc;
+            this.auteur = auteur;
+            this.video = video;
+            this.rating = rating;
+            this.dieet = dieet;
+            this.benodigdheden = benodigdheden;
+            this.image = image;
+            this.ingredienten = ingredienten;
+            this.stappen = stappen;
+        }
     }
 }
