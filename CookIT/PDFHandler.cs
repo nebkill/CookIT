eef7a469f61d6583f02439cee1bcef6bed4d0f2b
MichaJDevLaptop <vyxisprime@gmail.com>
2018-12-18 14:21:01 +0100

    Created PDFHandler
    Creatd PDFHandler to store opperations of PDF Generators

---
+++ CookIT/PDFHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookIT
+{
+
+    class PDFHandler
+    {
+
+        public void createPDF(Recept recept)
+        {
+        }
+    }
+
+
+}
