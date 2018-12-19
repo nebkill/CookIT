c809d7cab64e69c6e2d59067756a4bc5f6c2d677
Ben de Greef <ben.degreef@hotmail.com>
2018-12-19 10:24:22 +0100

    alle grotes goed gezet

--- CookIT/Module_ReceptList.Designer.cs
+++ CookIT/Module_ReceptList.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Module_ReceptList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Module_ReceptList";
+            this.Size = new System.Drawing.Size(1260, 630);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- CookIT/Module_ReceptView.Designer.cs
+++ CookIT/Module_ReceptView.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "Module_ReceptView";
-            this.Size = new System.Drawing.Size(800, 400);
+            this.Size = new System.Drawing.Size(1260, 630);
             this.ResumeLayout(false);
 
         }
