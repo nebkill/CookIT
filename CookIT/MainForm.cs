b46e213bb148d19800ca078fe9a8d738070d97e6
MichaJ <michajanssen93@gmail.com>
2019-01-03 15:00:51 +0100

    Update MainForm.cs
    Cleared Mainform from all the bs

--- CookIT/MainForm.cs
+++ CookIT/MainForm.cs
@@ -24,26 +24,11 @@ namespace CookIT
         Connect con = new Connect();
         PDFHandler pdf = new PDFHandler();
         //Constructor
-        public string[] benod= new string[] {"LOL","NEE","LMAO" };
-        public string[] ingre = new string[] {"ANDERS", "GEAARD","LOL"};
-        List<string> stappen = new List<string>();
-        List<string> ben = new List<string>();
-        List<string> ing = new List<string>();
+        
         public MainForm()
         {
             InitializeComponent();
-            foreach (string item in benod)
-            {
-                ben.Add(item);
-            }
-            foreach(string item in ingre)
-            {
-                ing.Add(item);
-            }
-            stappen.Add("LOL");
-            stappen.Add("Jemoeder");
-            Recept recept = new Recept(1, "Mike's Moeder", "Ze is Heel Dik", "COOKIT", @"C:\Iets\Video\", 0, "Low_Carb", ben,@"C:\Iets\iets" , ing, stappen);
-            pdf.createPDF(recept);
+            
 
 
 
