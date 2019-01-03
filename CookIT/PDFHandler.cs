323324b62004634549d9f2df0911c3ee6e51f80e
MichaJ <michajanssen93@gmail.com>
2019-01-03 14:54:36 +0100

    Update PDFHandler
    Updated PDFHandler
    Edited placement for Description
    Added Steps with checkboxes

--- CookIT/PDFHandler.cs
+++ CookIT/PDFHandler.cs
@@ -25,21 +25,30 @@ namespace CookIT
             XFont heading2Font = new XFont("Verdana", 14, XFontStyle.Bold);
             gfx.DrawString(recept.naam, font, XBrushes.Black, new XRect(0, 10, page.Width, 40), XStringFormats.TopCenter);
             gfx.DrawString("Auteur: " + recept.auteur, smallFont, XBrushes.Black, new XRect(20, 10, page.Width, 40), XStringFormats.TopLeft);
-            gfx.DrawString("Description: " + recept.desc, smallFont, XBrushes.Black, new XRect(20, 22, page.Width, 40), XStringFormats.TopLeft);
+            
             gfx.DrawString("_________________________________________________________________________________________________________", smallFont, XBrushes.Black, new XRect(0, 30, page.Width, 40), XStringFormats.TopCenter);
-            gfx.DrawString("Benodigdheden", heading2Font, XBrushes.Black, new XRect(20, 50, page.Width, 40), XStringFormats.TopLeft);
-            int listHeight = 70;
+            gfx.DrawString("Description: " + recept.desc, smallFont, XBrushes.Black, new XRect(20, 50, page.Width, 40), XStringFormats.TopLeft);
+            int listHeight = 100;
+            gfx.DrawString("Benodigdheden", heading2Font, XBrushes.Black, new XRect(20, 100, page.Width, page.Height), XStringFormats.TopLeft);
+            listHeight = listHeight + 20;
             for (int i = 0; i < recept.benodigdheden.Count; i++)
             {
                 gfx.DrawString(" - " + recept.benodigdheden[i], smallFont, XBrushes.Black, new XRect(20, listHeight, page.Width, 40), XStringFormats.TopLeft);
                 listHeight += 12;
             }
-            listHeight = 70;
-            gfx.DrawString("Ingredienten", heading2Font, XBrushes.Black, new XRect(300, 50, page.Width, 40), XStringFormats.TopLeft);
-            
+            listHeight = 120;
+            gfx.DrawString("Ingredienten", heading2Font, XBrushes.Black, new XRect(300, 100, page.Width, 40), XStringFormats.TopLeft);
             for (int i = 0; i < recept.ingredienten.Count; i++)
             {
                 gfx.DrawString("-" + recept.ingredienten[i], smallFont, XBrushes.Black, new XRect(310, listHeight, page.Width, 40), XStringFormats.TopLeft);
+                listHeight += 12;
+            }
+            listHeight = listHeight + 20;
+            gfx.DrawString("Stappen", heading2Font, XBrushes.Black, new XRect(20, listHeight, page.Width, page.Height), XStringFormats.TopLeft);
+            listHeight = listHeight + 40;
+            for (int i = 0; i < recept.stappen.Count; i++)
+            {
+                gfx.DrawString("O     " + recept.stappen[i], smallFont, XBrushes.Black, new XRect(40, listHeight, page.Width, 40), XStringFormats.TopLeft);
                 listHeight += 12;
             }
             string root = @"C:\CookIT";
@@ -49,10 +58,12 @@ namespace CookIT
             {
                 Directory.CreateDirectory(root);
             }
-            string filename = @"C:\CookIt\" + recept.naam + "Recept.pdf";
+            string filename = @"C:\CookIt\" + recept.naam + " Recept.pdf";
             try
             {
                 doc.Save(filename);
+                MessageBox.Show("Succesfully Created "+  recept.naam +" Recept.pdf \n" +
+                    "Location:  " + filename + "" );
             }
             catch (IOException io)
             {
@@ -60,5 +71,7 @@ namespace CookIT
             }
             Process.Start(filename);
         }
+
+        
     }
 }
